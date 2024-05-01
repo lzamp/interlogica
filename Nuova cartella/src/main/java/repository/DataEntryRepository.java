2be813e13702e3e67eaf82dc5247ad02693132db
linda.zampieri <gg.xx.28@gmail.com>
2024-05-01 21:47:38 +0200

    Modifica nome

--- Nuova cartella/src/main/java/repository/DataEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.DataEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataEntryRepository extends JpaRepository<DataEntry, Long>{
-}
